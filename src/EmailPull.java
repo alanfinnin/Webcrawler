e908e41fca7abfffd25db2c0b13e45a05b027f4f
Alan Finnin <37178050+alanfinnin@users.noreply.github.com>
2018-12-20 20:56:17 +0000

    Checks and formats emails (unfinished)
    This takes the line and narrows it down in the hope of finding an email within it
    Doesnt work fully right now as it grabs false flags (not emails but contains some of the requirments to be one)
    and doesnt recognise ".ie" for some reason

---
+++ src/EmailPull.java
@@ -0,0 +1,25 @@
+if(line.contains("@") && (line.contains(".com") || line.contains(".net")))
+				{
+					if(line.contains(" "))
+					{
+						if(line.contains(".com"))
+							email = line.substring(line.indexOf(" "), line.indexOf(".com")+4);
+						else if(line.contains(".net"))
+							email = line.substring(line.indexOf(" "), line.indexOf(".net")+4);
+						else if(line.contains(".ie"))
+							email = line.substring(line.indexOf(" "), line.indexOf(".ie")+3);
+						email = (email.substring(email.lastIndexOf(" "), email.length()).trim());
+						if(email.contains("@") && (email.contains(".com") || email.contains(".net") || email.contains(".ie")))
+							emailArray.add(email);
+					}
+					else
+					{
+						if(line.contains(".com"))
+							email = line.substring(0, line.indexOf(".com")+4);
+						else if(line.contains(".net"))
+							email = line.substring(0, line.indexOf(".net")+4);
+						else if(line.contains(".ie"))
+							email = line.substring(line.indexOf(" "), line.indexOf(".ie")+3);
+						if(email.contains("@") && (email.contains(".com") || email.contains(".net") || email.contains(".ie")))
+							emailArray.add(email);
+					}
